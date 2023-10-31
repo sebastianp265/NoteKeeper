45fa1bfa9685eb3988bbacabd8c114794bdfcaaf
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-10-31 14:02:09 +0100

    Add note entity

--- backend/src/main/java/com/github/sebastianp265/notekeeper/entities/Note.java
+++ backend/src/main/java/com/github/sebastianp265/notekeeper/entities/Note.java
@@ -1,4 +1,14 @@
 package com.github.sebastianp265.notekeeper.entities;
 
+import jakarta.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+
 public class Note {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String text;
+
 }
