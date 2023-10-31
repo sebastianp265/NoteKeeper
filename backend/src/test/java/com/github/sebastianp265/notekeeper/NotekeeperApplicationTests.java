a6266d9a20bc742ba9bc3d89775e8118435a0911
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-10-31 13:29:51 +0100

    Spring initialization

---
+++ backend/src/test/java/com/github/sebastianp265/notekeeper/NotekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sebastianp265.notekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
