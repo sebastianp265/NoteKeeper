b89845a091593fd9b4bcd1e75072e062d905b128
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-10-31 13:55:35 +0100

    Adding JpaRepository dependency

--- backend/src/main/java/com/github/sebastianp265/notekeeper/repositories/NoteRepository.java
+++ backend/src/main/java/com/github/sebastianp265/notekeeper/repositories/NoteRepository.java
@@ -3,6 +3,6 @@ package com.github.sebastianp265.notekeeper.repositories;
 import com.github.sebastianp265.notekeeper.entities.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoteRepository {
+public interface NoteRepository extends JpaRepository<Note, Long> {
 
 }
