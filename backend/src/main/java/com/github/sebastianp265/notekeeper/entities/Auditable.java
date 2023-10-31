9832fdd94b4c73d22f1d085edc7d0761965e141b
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-10-31 20:32:06 +0100

    fixing Auditable.java class annotations

--- backend/src/main/java/com/github/sebastianp265/notekeeper/entities/Auditable.java
+++ backend/src/main/java/com/github/sebastianp265/notekeeper/entities/Auditable.java
@@ -1,11 +1,24 @@
 package com.github.sebastianp265.notekeeper.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable {
 
     @CreatedDate
