4a0d52fa8f829521c0af739326a33d1d68c733ec
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-11-01 13:51:21 +0100

    Added OpenAPI with swagger

---
+++ backend/src/main/java/com/github/sebastianp265/notekeeper/configurations/OpenAPIConfiguration.java
@@ -0,0 +1,24 @@
+package com.github.sebastianp265.notekeeper.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NoteKeeper backend REST API")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Sebastian Pawliński")
+                                .email("pawlinskisebastian@gmail.com")
+                                .url("github.com/sebastianp265"))
+                );
+    }
+}
