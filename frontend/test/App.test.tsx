01cb27720d010ed821e045dc68b643413b56efcc
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-11-16 13:15:31 +0100

    Reinit project using vite instead of create-react-app due to issues with working tailwindcss with postcss and autoprefixer

--- frontend/test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import LearnReact from '../src/pages/learn_react/LearnReact';
-
-test('renders learn react link', () => {
-    render(<LearnReact/>);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
